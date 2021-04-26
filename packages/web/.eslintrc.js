8ecac9b0973b75cc2f138c7c1c4484d78a4dc461
Alireza Safaierad <frontendmonster@gmail.com>
2021-04-26 15:02:13 +0430

    feat: init monorepo (#5)

---
+++ packages/web/.eslintrc.js
@@ -0,0 +1,4 @@
+module.exports = {
+  extends: ['../../.eslintrc.js', '@fullstacksjs/eslint-config/react'],
+  root: true,
+};
---
+++ packages/web/src/App.tsx
@@ -0,0 +1,3 @@
+const App: React.FC = () => <h1>Codeit</h1>;
+
+export default App;
