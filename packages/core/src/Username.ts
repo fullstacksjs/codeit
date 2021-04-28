6055f1dc30951c3d64ce4cc573fd8ca98c248237
AmirabbasJ <amirabbasgamer2@gmail.com>
2021-04-28 18:32:52 +0430

    feat: add username model

---
+++ packages/core/src/Username.ts
@@ -0,0 +1,11 @@
+import * as t from 'io-ts';
+
+interface UsernameBrand {
+  readonly Username: unique symbol;
+}
+
+export type Username = t.Branded<string, UsernameBrand>;
+
+const isValid = (v: string): v is Username => v.length > 5 && v.length < 25;
+
+export const Username = t.brand(t.string, isValid, 'Username');
