efe99775a9e2166cb75850bc8c4ca0fa4b0ce56c
AmirabbasJ <amirabbasgamer2@gmail.com>
2021-04-28 18:08:09 +0430

    feat: add id field for testcase model

--- packages/core/src/TestCase.ts
+++ packages/core/src/TestCase.ts
@@ -1,9 +1,11 @@
 import * as t from 'io-ts';
+import * as tt from 'io-ts-types';
 
 import { TestCaseMode } from './TestCaseMode';
 import { Title } from './Title';
 
 export const TestCase = t.type({
+  id: tt.NonEmptyString,
   title: Title,
   input: t.string,
   assertion: t.string,
