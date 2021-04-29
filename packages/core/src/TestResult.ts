fc7bcb82131c5faea383f7dcb1b6f0742ad30e9f
AmirabbasJ <amirabbasgamer2@gmail.com>
2021-04-29 18:11:54 +0430

    feat: add test result status

--- packages/core/src/TestResult.ts
+++ packages/core/src/TestResult.ts
@@ -2,8 +2,10 @@ import * as t from 'io-ts';
 
 import { TestCase } from './TestCase';
 import { TestOutput } from './TestOutput';
+import { TestResultStatus } from './TestResultStatus';
 
 export const TestResult = t.type({
   testCase: TestCase,
   outputs: t.array(TestOutput),
+  status: TestResultStatus,
 });
