a847ae77b8c4047c27ce1b104c86233b7aad66c4
AmirabbasJ <amirabbasgamer2@gmail.com>
2021-06-04 11:07:58 +0430

    fix: attach context to the apolloServer instance

--- packages/api/src/ApiServer.ts
+++ packages/api/src/ApiServer.ts
@@ -2,8 +2,9 @@ import { ApolloServer } from 'apollo-server';
 
 import { Config } from './config/getConfig';
 import { createSchema } from './schema';
+import { createContext } from './schema/createContext';
 
 export function ApiServer(_config: Config) {
-  const apolloServer = new ApolloServer({ schema: createSchema() });
+  const apolloServer = new ApolloServer({ schema: createSchema(), context: createContext() });
   return apolloServer;
 }
