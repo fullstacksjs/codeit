a9580d0aa11e98f40e21c51c76530cfe52e5ed3e
AmirabbasJ <amirabbasgamer2@gmail.com>
2021-07-14 16:35:09 +0430

    fix: fix username regex

--- packages/core/src/Username.ts
+++ packages/core/src/Username.ts
@@ -7,6 +7,6 @@ interface UsernameBrand {
 export type Username = t.Branded<string, UsernameBrand>;
 
 const isValid = (v: string): v is Username =>
-  v.length > 5 && v.length < 25 && /[^A-Za-z0-9]/.test(v);
+  v.length > 5 && v.length < 25 && /^[a-zA-Z0-9_]+$/.test(v);
 
 export const Username = t.brand(t.string, isValid, 'Username');
