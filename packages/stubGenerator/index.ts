ca848582048be89f3f3ead2c93d9656b5e15856a
AmirabbasJ <amirabbasgamer2@gmail.com>
2021-09-02 01:30:09 +0430

    fix: comment js parsing for typescript

--- packages/stubGenerator/index.ts
+++ packages/stubGenerator/index.ts
@@ -6,7 +6,7 @@ import { generateJavascriptStub } from './languages/javascript/src';
 export const generateStub = (language: Language, syntax: string): string =>
   match(language)
     .with('JavaScript', () => generateJavascriptStub(syntax))
-    .with('TypeScript', () => generateJavascriptStub(syntax))
+    // .with('TypeScript', () => generateJavascriptStub(syntax)) this would work too
     // .with('F#', () => fsharpGenerate(syntax))
     // .with('Go', () => goGenerate(syntax))
     .otherwise(() => '');
