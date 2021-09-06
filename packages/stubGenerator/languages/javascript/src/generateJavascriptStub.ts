d99b9bc62d96c894caaba72f54fba2e4a13fcd8b
AmirabbasJ <amirabbasgamer2@gmail.com>
2021-09-06 13:01:39 +0430

    fix(stub:js): add todo to put util as helpers

--- packages/stubGenerator/languages/javascript/src/generateJavascriptStub.ts
+++ packages/stubGenerator/languages/javascript/src/generateJavascriptStub.ts
@@ -1,6 +1,8 @@
 import { parseLine } from './parseLine';
 
 export const generateJavascriptStub = (input: string) => {
+  // TODO: put utils as global helpers
+
   const utils = `const toInt = x => Number.parseInt(x, 10);
 const toFloat = x => Number.parseFloat(x, 10);
 const range = n => [...Array(n)].map((_, i) => i);\n\n`;
