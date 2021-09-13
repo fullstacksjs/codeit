6e9e3a68eb00466d801ad3988e058ebc8a85cd1b
AmirabbasJ <amirabbasgamer2@gmail.com>
2021-09-14 01:49:48 +0430

    fix(stub): fix stub generator to parse before generating

--- packages/stubGenerator/index.ts
+++ packages/stubGenerator/index.ts
@@ -1,9 +1,13 @@
+import { pipe } from 'fp-ts/lib/function';
 import { match } from 'ts-pattern';
 
 import { Language } from '../core';
 import { generateJavascriptStub } from './javascript';
+import { parseSyntax } from './syntaxParser/parseSyntax';
 
 export const generateStub = (language: Language, syntax: string): string =>
-  match(language)
-    .with('JavaScript', () => generateJavascriptStub(syntax))
-    .otherwise(() => '');
+  pipe(syntax, parseSyntax, s =>
+    match(language)
+      .with('JavaScript', () => generateJavascriptStub(s))
+      .otherwise(() => ''),
+  );
