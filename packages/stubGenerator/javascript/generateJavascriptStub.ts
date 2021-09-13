f835c39fc53de3b0ddcc14c6f870b7120bec498a
AmirabbasJ <amirabbasgamer2@gmail.com>
2021-09-14 01:49:07 +0430

    fix(stub:js): fix generator to rely on high-level domain

--- packages/stubGenerator/javascript/generateJavascriptStub.ts
+++ packages/stubGenerator/javascript/generateJavascriptStub.ts
@@ -1,11 +1,12 @@
+import { Syntax } from '../../core';
 import { parseLine } from './parseLine';
 
-export const generateJavascriptStub = (input: string) => {
+export const generateJavascriptStub = (input: Syntax) => {
   // TODO: put utils as global helpers
 
   const utils = `const toInt = x => Number.parseInt(x, 10);
 const toFloat = x => Number.parseFloat(x, 10);
 const range = n => [...Array(n)].map((_, i) => i);\n\n`;
 
-  return utils + input.trim().split('\n').map(parseLine).join('\n');
+  return utils + input.map(parseLine).join('\n');
 };
--- packages/stubGenerator/javascript/parseLine.ts
+++ packages/stubGenerator/javascript/parseLine.ts
@@ -1,5 +1,6 @@
-import { match } from 'ts-pattern';
+import { __, match } from 'ts-pattern';
 
+import { Instruction, Loop } from '../../core';
 // eslint-disable-next-line import/no-cycle
 import {
   parseLoopCommand,
@@ -8,12 +9,16 @@ import {
   parseWriteCommand,
 } from './commandParsers';
 
-export const parseLine = (line: string) => {
-  const [command, ...rest] = line.trim().split(' ');
-  return match(command)
-    .with('read', () => parseReadCommand(rest))
-    .with('loopline', () => parseLooplineCommand(rest))
-    .with('loop', () => parseLoopCommand(rest))
-    .with('write', () => parseWriteCommand(rest))
-    .otherwise(() => '');
+export const parseLine = (instruction: Instruction) => {
+  // thanks u typescript for fucking up my pattern match ❤️
+  if (instruction[0] === 'loop') return parseLoopCommand(instruction as Loop);
+
+  return (
+    match(instruction)
+      .with(['read', __], parseReadCommand)
+      .with(['loopline', __, __], parseLooplineCommand)
+      .with(['write', __], parseWriteCommand)
+      // .with(['loop', __], x => parseLoopCommand(x as string[]))
+      .otherwise(() => '')
+  );
 };
