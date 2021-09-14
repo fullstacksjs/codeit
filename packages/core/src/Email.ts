db9e94d98ab8845864af893f9a3ad0479889df12
AmirabbasJ <47698362+AmirabbasJ@users.noreply.github.com>
2021-09-14 11:54:41 +0430

    feat: add missing model properties (#13)

---
+++ packages/core/src/Email.ts
@@ -0,0 +1,13 @@
+import * as t from 'io-ts';
+
+interface EmailBrand {
+  readonly Email: unique symbol;
+}
+
+const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const validateEmail = (email: string): email is t.Branded<string, EmailBrand> =>
+  emailRegex.test(email);
+
+const Email = t.brand(t.string, validateEmail, 'Email');
+
+export { Email };
--- packages/core/src/Player.ts
+++ packages/core/src/Player.ts
@@ -1,11 +1,13 @@
 import * as t from 'io-ts';
 import * as tt from 'io-ts-types';
 
+import { Email } from './Email';
 import { Id } from './Id';
 import { Username } from './Username';
 
 export const Player = t.type({
   id: Id,
   username: Username,
+  email: Email,
   displayName: tt.NonEmptyString,
 });
