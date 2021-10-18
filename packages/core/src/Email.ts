148e48b17d8ac8ce386f8d84f10fee7d751025f2
Alireza Safaierad <frontendmonster@gmail.com>
2021-10-18 20:12:12 +0400

    chore(stubGenerator): remove @codeit/core from package.json

--- packages/core/src/Email.ts
+++ packages/core/src/Email.ts
@@ -1,10 +1,12 @@
 import * as t from 'io-ts';
 
-interface EmailBrand {
+export interface EmailBrand {
   readonly Email: unique symbol;
 }
 
-const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const emailRegex =
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
 const validateEmail = (email: string): email is t.Branded<string, EmailBrand> =>
   emailRegex.test(email);
 
