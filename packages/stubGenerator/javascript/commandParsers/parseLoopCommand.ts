04e2fbca57e6d2a3fd48f59bdd144792614a7620
AmirabbasJ <amirabbasgamer2@gmail.com>
2021-10-18 18:12:11 +0330

    refactor: remove binding

--- packages/stubGenerator/javascript/commandParsers/parseLoopCommand.ts
+++ packages/stubGenerator/javascript/commandParsers/parseLoopCommand.ts
@@ -1,13 +1,19 @@
-import type { Loop } from '../../../core';
+import type { Loop } from '@codeit/core';
+import { map } from 'fp-ts/lib/Array';
+import { pipe } from 'fp-ts/lib/function';
+import { join, split } from 'ramda';
+
 // eslint-disable-next-line import/no-cycle
 import { parseLine } from '../parseLine';
 
-export const parseLoopCommand = ([_, amount, instruction]: Loop): string => {
-  const innerInstruction = parseLine(instruction)
-    .split('\n')
-    .map(i => `  ${i}`)
-    .join('\n');
-  return `range(${amount}).forEach(() => {
-${innerInstruction}
-});`;
-};
+export const parseLoopCommand = ([_, amount, instruction]: Loop): string =>
+  pipe(
+    parseLine(instruction),
+    split('\n'),
+    map(i => `  ${i}`),
+    join('\n'),
+    parsedInstruction =>
+      `range(${amount}).forEach(() => {
+${parsedInstruction}
+});`,
+  );
