e9903452984642359a5ce5ba92a1d6b6b06f6f57
AmirabbasJ <amirabbasgamer2@gmail.com>
2021-10-18 16:44:14 +0330

    refactor: make variables in Read type a nonEmptyArray

--- packages/core/src/stubGenerator.ts
+++ packages/core/src/stubGenerator.ts
@@ -1,9 +1,10 @@
+import { NonEmptyArray } from 'fp-ts/NonEmptyArray';
+
 export type VarType = 'float' | 'int' | 'string' | 'word';
 export type Variable = [string, VarType];
-
 export type Loop = ['loop', string, Instruction];
 export type Loopline = ['loopline', string, Variable];
-export type Read = ['read', Variable[]];
+export type Read = ['read', NonEmptyArray<Variable>];
 export type Write = ['write', string];
 
 export type Instruction = Loop | Loopline | Read | Write;
