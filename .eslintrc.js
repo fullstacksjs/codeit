7227d26a36cbb8ed109d3c2a9c15a541d7d3ae1c
Alireza Safaierad <frontendmonster@gmail.com>
2021-10-19 12:49:34 +0400

    fix(eslint): fix ts alias import eslint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,14 @@
 module.exports = {
   root: true,
   extends: ['@fullstacksjs'],
+  rules: {
+    'import/no-extraneous-dependencies': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        project: 'tsconfig.base.json',
+      },
+    },
+  },
 };
