c7f2de815bbcd70e097646613734a64326e2d6d2
AmirabbasJ <amirabbasgamer2@gmail.com>
2021-11-04 18:16:46 +0330

    fix: fix player fields to be nonNull

--- apps/api/src/schema/modules/Player.ts
+++ apps/api/src/schema/modules/Player.ts
@@ -7,9 +7,9 @@ export const Player = objectType({
     export: 'Player',
   },
   definition(t) {
-    t.string('id', { resolve: s => s.id });
-    t.string('displayName', { resolve: s => s.displayName });
-    t.string('email', { resolve: s => s.email });
-    t.string('username', { resolve: s => s.username });
+    t.nonNull.string('id', { resolve: s => s.id });
+    t.nonNull.string('displayName', { resolve: s => s.displayName });
+    t.nonNull.string('email', { resolve: s => s.email });
+    t.nonNull.string('username', { resolve: s => s.username });
   },
 });
