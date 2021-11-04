d44ad3731498aa950923db30e8e63d130fd1f5ba
AmirabbasJ <amirabbasgamer2@gmail.com>
2021-11-04 18:13:40 +0330

    feat: add player to query

--- apps/api/src/schema/modules/Query.ts
+++ apps/api/src/schema/modules/Query.ts
@@ -1,11 +1,24 @@
-import { queryType } from 'nexus';
+import { Id } from '@codeit/core';
+import { pipe } from 'fp-ts/function';
+import * as TO from 'fp-ts/TaskOption';
+import { toNullable } from 'fp-ts-contrib/TaskOption';
+import { idArg, nonNull, queryType } from 'nexus';
 
 export const Query = queryType({
   definition(t) {
-    t.nullable.string('name');
-    t.list.field('users', {
-      type: 'User',
-      resolve: (_, __, context) => context.prisma.user.findMany({}),
+    t.field('player', {
+      type: 'Player',
+      args: {
+        id: nonNull(idArg()),
+      },
+      resolve: (_, { id }, context) =>
+        pipe(
+          id,
+          Id.decode,
+          TO.fromEither,
+          TO.chain(context.playerRepo.getPlayerById),
+          toNullable,
+        )(),
     });
   },
 });
