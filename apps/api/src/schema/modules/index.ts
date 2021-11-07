bbe3237bd31cec68d55dea4773ea6542485b978a
AmirabbasJ <47698362+AmirabbasJ@users.noreply.github.com>
2021-11-07 14:23:12 +0330

    feat: initiate nexus (#31)

--- apps/api/src/schema/modules/index.ts
+++ apps/api/src/schema/modules/index.ts
@@ -1,4 +1,8 @@
 export * from './Player';
+<<<<<<< HEAD
 export * from './Puzzle';
 export * from './Query';
 export * from './TestCase';
+=======
+export * from './Query';
+>>>>>>> de6989e (feat: initiate nexus (#31))
