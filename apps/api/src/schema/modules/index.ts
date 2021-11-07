52639789648eb4e552a0600101ef15b397e93b03
AmirabbasJ <amirabbasgamer2@gmail.com>
2021-11-07 15:49:54 +0330

    fix: fix module exportation

--- apps/api/src/schema/modules/index.ts
+++ apps/api/src/schema/modules/index.ts
@@ -1,8 +1,4 @@
 export * from './Player';
-<<<<<<< HEAD
 export * from './Puzzle';
 export * from './Query';
 export * from './TestCase';
-=======
-export * from './Query';
->>>>>>> de6989e (feat: initiate nexus (#31))
