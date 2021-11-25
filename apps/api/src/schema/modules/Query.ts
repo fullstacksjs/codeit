19c0aa9661f273413a774a0d322eb1f586db4a7f
AmirabbasJ <amirabbasgamer2@gmail.com>
2021-11-25 15:32:38 +0330

    refactor: rename query names

--- apps/api/src/schema/modules/Query.ts
+++ apps/api/src/schema/modules/Query.ts
@@ -1,14 +1,14 @@
-import { Id, PuzzleMode } from '@codeit/core';
+import { Id, Title } from '@codeit/core';
 import { pipe } from 'fp-ts/function';
 import * as TO from 'fp-ts/TaskOption';
 import { toNullable } from 'fp-ts-contrib/TaskOption';
-import { arg, idArg, nonNull, queryType } from 'nexus';
+import { arg, idArg, nonNull, queryType, stringArg } from 'nexus';
 
 import { PuzzleModeArg } from './argTypes';
 
 export const Query = queryType({
   definition(t) {
-    t.field('player', {
+    t.field('getPlayerById', {
       type: 'Player',
       args: {
         id: nonNull(idArg()),
@@ -22,17 +22,44 @@ export const Query = queryType({
           toNullable,
         )(),
     });
-    t.field('puzzle', {
+    t.field('getRandomPuzzle', {
+      type: 'Puzzle',
+      resolve: (_, __, context) =>
+        pipe(context.puzzleRepo.getRandomPuzzle(), toNullable)(),
+    });
+    t.field('getPuzzleByTitle', {
+      type: 'Puzzle',
+      args: {
+        title: nonNull(stringArg()),
+      },
+      resolve: (_, { title }, context) =>
+        pipe(
+          title,
+          Title.decode,
+          TO.fromEither,
+          TO.chain(context.puzzleRepo.getPuzzleByTitle),
+          toNullable,
+        )(),
+    });
+    t.field('getRandomPuzzleByMode', {
       type: 'Puzzle',
       args: {
         mode: nonNull(arg({ type: PuzzleModeArg })),
       },
       resolve: (_, { mode }, context) =>
+        pipe(context.puzzleRepo.getRandomPuzzleByMode(mode), toNullable)(),
+    });
+    t.field('getPuzzleById', {
+      type: 'Puzzle',
+      args: {
+        id: nonNull(idArg()),
+      },
+      resolve: (_, { id }, context) =>
         pipe(
-          mode,
-          PuzzleMode.decode,
+          id,
+          Id.decode,
           TO.fromEither,
-          TO.chain(context.puzzleRepo.getRandomPuzzleByMode),
+          TO.chain(context.puzzleRepo.getPuzzleById),
           toNullable,
         )(),
     });
