c9185ed6d27a691872e60720d37a4e50137d1351
Alireza Safaierad <frontendmonster@gmail.com>
2022-02-07 23:44:47 +0400

    refactor(playground): rename app to playground

--- apps/playground/src/App/Playground.spec.tsx
+++ apps/playground/src/App/Playground.spec.tsx
@@ -1,10 +1,10 @@
 import { render } from '@testing-library/react';
 
-import { App } from './App';
+import { Playground } from './Playground';
 
 describe('App', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<App />);
+    const { baseElement } = render(<Playground />);
 
     expect(baseElement).toBeTruthy();
   });
---
+++ apps/playground/src/App/Playground.tsx
@@ -0,0 +1,1 @@
+export const Playground: React.FC = () => <h1>Codeit</h1>;
